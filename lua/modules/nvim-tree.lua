97fd3011067762998d5c76480151b56506ad78ff
serii <seriiburduja@gmail.com>
2024-02-27 09:52:51 +0200

    feat: nvim tree open

--- lua/modules/nvim-tree.lua
+++ lua/modules/nvim-tree.lua
@@ -12,24 +12,39 @@ vim.cmd([[
     :hi link NvimTreeImageFile   Title
 ]])
 
+local function my_on_attach(bufnr)
+	local api = require("nvim-tree.api")
+
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	-- default mappings
+	api.config.mappings.default_on_attach(bufnr)
+
+	-- custom mappings
+	vim.keymap.set("n", "l", api.node.open.edit, opts("Help"))
+end
+
 -- empty setup using defaults
 require("nvim-tree").setup()
 
 -- OR setup with some options
 require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 50,
-    -- float = {
-    --     enable = true,
-    -- },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+	sort = {
+		sorter = "case_sensitive",
+	},
+	view = {
+		width = 50,
+		-- float = {
+		--     enable = true,
+		-- },
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
+	on_attach = my_on_attach,
 })
